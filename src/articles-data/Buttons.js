e72a7c1e8c5237768a31d3c4a9eb55506ee5fded
mrizkitriyanto <muhamadrizkitriyanto41@gmail.com>
2021-07-12 23:18:58 +0700

    menambah comment

--- src/articles-data/Buttons.js
+++ src/articles-data/Buttons.js
@@ -6,7 +6,7 @@ function Buttons({button, filter}) {
             {
                 // Membuat buttons otomatis menggunakan Set yang ada di ArticleContent.js
                 button.map((cat, i)=>{
-                    return <button className="bg-yellow-100 text-gray-900 dark:bg-gray-800 dark:text-gray-300 mx-2 my-3 px-3 py-1 rounded-lg hover:bg-yellow-50 dark:hover:bg-gray-900 transition duration-1000" type="button" onClick={()=> filter(cat)}>{cat}</button>
+                    return <button className="bg-yellow-100 text-gray-900 dark:bg-gray-800 dark:text-gray-300 font-mono mx-2 my-3 px-3 py-1 rounded-lg hover:bg-yellow-50 dark:hover:bg-gray-900 transition duration-1000" type="button" onClick={()=> filter(cat)}>{cat}</button>
                 })
             }
         </div>
